b313427160d37eeb21194509b883a7431fc227cf
Motoki Nakae <motoki@cleverdog.me>
2022-05-25 20:27:58 +0900

    IG Landing Page の更新

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './error-routing/not-found/not-found.component';
 import { UncaughtErrorComponent } from './error-routing/error/uncaught-error.component';
 import { ErrorRoutingModule } from './error-routing/error-routing.module';
+import { UitoolsComponent } from './uitools/uitools.component';
 import { Test1Component } from './test1/test1.component';
-import { Test2Component } from './test2/test2.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'test1', pathMatch: 'full' },
+  { path: '', redirectTo: 'uitools', pathMatch: 'full' },
   { path: 'error', component: UncaughtErrorComponent },
+  { path: 'uitools', component: UitoolsComponent, data: { text: 'uitools' } },
   { path: 'test1', component: Test1Component, data: { text: 'test1' } },
-  { path: 'test2', component: Test2Component, data: { text: 'test2' } },
   { path: '**', component: PageNotFoundComponent } // must always be last
 ];
 
---
+++ src/app/uitools/uitools.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uitools',
+  templateUrl: './uitools.component.html',
+  styleUrls: ['./uitools.component.scss']
+})
+export class UitoolsComponent {
+}
