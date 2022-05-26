ec824e67a1a367d31f6502847aad777092954a93
MotokiNakae <motoki@cleverdog.me>
2022-05-26 22:41:23 +0900

    カスタムスタイルの追加

--- src/app/uitools/uitools.component.ts
+++ src/app/uitools/uitools.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uitools',
   templateUrl: './uitools.component.html',
-  styleUrls: ['./uitools.component.scss']
+  styleUrls: ['./uitools.component.scss','./uitools.component.add.scss']
 })
 export class UitoolsComponent {
+
+  // #TODO メタタグ書き換え
+
 }
