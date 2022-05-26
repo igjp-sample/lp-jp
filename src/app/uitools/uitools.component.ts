011c10a39f7a01af651fd0dfa16e4c2d26ce5cc7
Motoki Nakae <motoki@cleverdog.me>
2022-05-26 23:16:01 +0900

    IG Landing Page の更新

--- src/app/uitools/uitools.component.ts
+++ src/app/uitools/uitools.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uitools',
   templateUrl: './uitools.component.html',
-  styleUrls: ['./uitools.component.scss','./uitools.component.add.scss']
+  styleUrls: ['./uitools.component.scss']
 })
 export class UitoolsComponent {
-
-  // #TODO メタタグ書き換え
-
 }
